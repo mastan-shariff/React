c8145a800ef41f602494c5136a1fcdcae31b4d28
shariff <mastanshariff17@gmail.com>
2024-01-03 14:22:10 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,23 @@
+const heading = React.createElement('h1', { id: "red" }, "Hello world react");
+const parent = React.createElement(
+    'div',
+    { id: "parent" },
+    [React.createElement(
+        'div',
+        { id: "child1",style:{color:"red"} },
+        [
+            React.createElement('h1', {}, "i am a nested h1 inside child and child is in parent"),
+            React.createElement('h2', {}, "i am a nested h1 inside child and child is in parent"),
+        ]), 
+        React.createElement(
+            'div',
+            { id: "child2" },
+            [
+                React.createElement('h1', {}, "i am a nested h1 inside child and child is in parent"),
+                React.createElement('h2', {}, "i am a nested h1 inside child and child is in parent"),
+            ])
+
+     ])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
